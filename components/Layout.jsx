e34c90b0d80329cdeeb186a35527fba332c91339
jose martinez <che.manuel758@gmail.com>
2023-12-04 17:57:28 -0400

    test clerk

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+// import { ClerkProvider } from '@clerk/nextjs'
 import Header from './Header';
 import Footer from './Footer';
 
 const Layout = ({ children }) => (
   <>
+    {/* <ClerkProvider> */}
     <Header />
     {children}
     <Footer />
+    {/* </ClerkProvider> */}
   </>
 );
 
